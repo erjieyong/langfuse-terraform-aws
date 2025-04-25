2475dc9afc38d0c766a613610285bc0c7e601290
erjieyong <erjieyong@gmail.com>
2025-04-25 16:58:50 +0800

    include terraform installation shell script

---
+++ install_terraform.sh
@@ -0,0 +1,5 @@
+TERRAFORM_VERSION="1.11.4"  # Replace with desired version
+wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+terraform -version
